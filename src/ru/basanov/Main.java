6cdbfa5ee69b1a74f033f414a4f0d26e26cd2da3
Mingiyan1987 <basanov.mt87@gmail.com>
2020-01-11 15:12:14 +0300

    Домашнее задание 2 урока.

--- src/ru/basanov/Main.java
+++ src/ru/basanov/Main.java
@@ -1,4 +1,25 @@
 package ru.basanov;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Main {
+    public static void main(String[] args) {
+        String string = "first";
+        String string1 = "one";
+        String string2 = "two";
+        MyLinkedList<String> stringMyLinkedList = new MyLinkedList<>();
+        stringMyLinkedList.add(string);
+        stringMyLinkedList.add(string1);
+        stringMyLinkedList.add(string2);
+        System.out.println(stringMyLinkedList.size);
+
+        MyArrayList<String> myArrayList = new MyArrayList();
+        ArrayList<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("t");
+        list.add("h");
+        myArrayList.addAll(0, list);
+        System.out.println(myArrayList.size());
+    }
 }
